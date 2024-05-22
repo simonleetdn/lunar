541e4b2bb9f9b30d321a78750032a6b97c8a308d
Simon <68718135+simonleetdn@users.noreply.github.com>
2024-05-23 01:06:30 +0800

    標題去掉黃曆

--- lunardate.php
+++ lunardate.php
@@ -14,7 +14,7 @@ if(isset($_GET["year-month"]) && !empty($_GET["year-month"])) {
 <body>
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-danger">
 	  <div class="container-md">
-    <a class="navbar-brand" href="lunardate.php">農民曆（黃曆）</a>
+    <a class="navbar-brand" href="lunardate.php">農民曆</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

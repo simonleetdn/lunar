c3596a325bb75baa9fc1aeaff47d548a26d08b63
Simon <68718135+simonleetdn@users.noreply.github.com>
2024-05-23 00:50:37 +0800

    改袁天罡稱骨算命

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			echo '<a href=lunardate.php?year-month='.$year.'-'.$month.' class="btn btn-warning mb-4">';
 			echo $year.'年'.$month.'月農民曆</a></br/>';
 			echo '<a href=yidate.php class="btn btn-warning mb-4">每年宜日速查</a><br/>';
-			echo '<a href=yuangang.php class="btn btn-warning">稱骨算命</a>';
+			echo '<a href=yuangang.php class="btn btn-warning">袁天罡稱骨算命</a>';
 			?>
           </div>
         </div>

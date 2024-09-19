70b7fa18b02deed749b0d3dcc6679318de88d140
Simon Lee <simon@ioi.tw>
2024-09-19 17:53:17 +0800

    加網頁導覽

--- footer.php
+++ footer.php
@@ -1,9 +1,42 @@
-<footer class="bg-danger text-light py-3 mt-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
+<footer class="bg-danger text-light py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <!-- Full-width title across 8 columns -->
+            <div class="col-md-12">
+                <h5 class="text-uppercase">網站導覽</h5>
+            </div>
+
+
+            <!-- Navigation links split into two columns -->
+            <div class="col-md-3">
+                    <a class="text-light" href="lunardate.php">農民曆首頁</a><br>
+                    <a class="text-light" href="yidate.php">每年宜日速查</a><br>
+                    
+            </div>
+            <div class="col-md-3">
+            <a class="text-light" href="/calendar/">日曆(自動更新)</a><br>
+            
+
+            </div>
+            <div class="col-md-3">
+                    
+            <a class="text-light" href="yuangang.php?birthDateTime=<?php echo date('Y-m-d\TH:i'); ?>">稱骨算命</a><br>
+
+            </div>
+            <div class="col-md-3">
+                    
+                    <a class="text-light" href="springox.php">春牛芒神服色</a>
+
+            </div>
+            </div>
+            <div class="row mt-4">
+            <div class="col-md-12">
+                <p>
                     網站設計：<a href="mailto:simon@ioi.tw" class="text-light">Simon Lee</a>
-                </div>
+                </p>
             </div>
         </div>
-    </footer>
+        
+    </div>
+
+</footer>
